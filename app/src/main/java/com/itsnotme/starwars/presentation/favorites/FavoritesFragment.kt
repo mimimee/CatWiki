1a9ffb9021233f66058fa478fa4451984e4207ad
Alexander Khasanov <alexander.kkkk@yandex.ru>
2022-02-05 18:14:35 +0400

    добавлен базовый фрагмент

--- app/src/main/java/com/itsnotme/starwars/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/itsnotme/starwars/presentation/favorites/FavoritesFragment.kt
@@ -2,21 +2,12 @@ package com.itsnotme.starwars.presentation.favorites
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
-import com.itsnotme.starwars.R
 import com.itsnotme.starwars.databinding.FragmentFavoritesBinding
+import com.itsnotme.starwars.presentation.base.BaseFragment
 
-class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
-    private var _binding: FragmentFavoritesBinding? = null
-    private val binding get() = _binding!!
+class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(FragmentFavoritesBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentFavoritesBinding.bind(view)
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
     }
 }
