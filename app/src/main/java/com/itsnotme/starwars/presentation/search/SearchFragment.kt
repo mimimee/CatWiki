f6b45ec4226e95caf8e5149635ccba836e2a7aec
Alexander Khasanov <alexander.kkkk@yandex.ru>
2022-02-05 18:24:15 +0400

    исправлен краш

--- app/src/main/java/com/itsnotme/starwars/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/itsnotme/starwars/presentation/search/SearchFragment.kt
@@ -2,21 +2,11 @@ package com.itsnotme.starwars.presentation.search
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
-import com.itsnotme.starwars.R
 import com.itsnotme.starwars.databinding.FragmentSearchBinding
+import com.itsnotme.starwars.presentation.base.BaseFragment
 
-class SearchFragment : Fragment(R.layout.fragment_search) {
-    private var _binding: FragmentSearchBinding? = null
-    private val binding get() = _binding!!
-
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentSearchBinding.bind(view)
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
     }
 }
